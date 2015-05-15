8f6b844dfa22b38bcf27325b541a87ea5801dfbb
minsikzzang <minsikzzang@gmail.com>
2015-05-16 00:06:33 +0100

    Add nightly build

--- app/scripts/controllers/settings.js
+++ app/scripts/controllers/settings.js
@@ -19,15 +19,60 @@ angular.module('webAdminApp')
       return null;
     }
 
+    $scope.merge_hours_text = function(hours) {
+      var text = [];
+      for (var key in hours) {
+        text.push(hours[key].text);
+      }
+      return text.join(", ");
+    }
+
+    $scope.getBusinessHour = function(authToken, businessId, siteApi) {
+      siteApi.hours(authToken, businessId).then(function(data) {
+        var hours = data.hours;
+        console.log(hours);
+        if (hours) {
+          $scope.hours = {};
+          var week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+          for (var key in week) {
+            $scope.hours[week[key]] = $scope.merge_hours_text(hours[week[key]]);
+          }
+        }
+
+      }, function(response) {
+        var message = 'Something bad happened :(';
+        if ((response.status == 401 || response.status == 422) && response.data && response.data.error) {
+          message = response.data.error.message;
+        }
+        // $scope.showAlert(message, 'danger', 'fa-warning');
+      });
+    }
+
+    $scope.getSiteBusiness = function(authToken, siteId, siteApi) {
+      siteApi.business(authToken, siteId).then(function(data) {
+        $scope.business = data.business;
+        $scope.getBusinessHour(authToken, $scope.business.id, siteApi);
+      }, function(response) {
+        var message = 'Something bad happened :(';
+        if ((response.status == 401 || response.status == 422) && response.data && response.data.error) {
+          message = response.data.error.message;
+        }
+        // $scope.showAlert(message, 'danger', 'fa-warning');
+      });
+    }
+
     $scope.authToken = storage.get("auth_token");
+    console.log("auth_token: " + $scope.authToken);
     if ($scope.getCurrentSite()) {
       $scope.site = $scope.getCurrentSite();
+      $scope.getSiteBusiness($scope.authToken, $scope.site.id, siteApi);
     } else {
       siteApi.index($scope.authToken).then(function(data) {
         $scope.sites = data.sites;
         if (data.sites.length > 0) {
           $rootScope.currentSite = data.sites[0];
           $scope.site = data.sites[0];
+          $scope.getSiteBusiness($scope.authToken, $scope.site.id, siteApi);
         }
       }, function(response) {
         var message = 'Something bad happened :(';
